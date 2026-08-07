6b6fbf6133e44d7069536d8e58d29f28f6476ed3
AllenYGY <allenygy@outlook.com>
2026-08-08 03:43:37 +0800

    overshelf 1.2.0

--- Casks/overshelf.rb
+++ Casks/overshelf.rb
@@ -1,6 +1,6 @@
 cask "overshelf" do
-  version "1.1.2"
-  sha256 "fcb48b82edbd7eb2ba1c407141186e00563e00b059ec0d1c6eccaa62708ff655"
+  version "1.2.0"
+  sha256 "7a080af2622bc2f80ac658438bb94f37117e4bfaac51c77e3a606f5c164a419d"
 
   url "https://github.com/ALLENYGY/overshelf/releases/download/v#{version}/OverShelf-#{version}.zip"
   name "OverShelf"
